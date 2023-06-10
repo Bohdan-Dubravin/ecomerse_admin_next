8f076b7764a4b6664d83328415ea485fa48a62ff
Bohdan-Dubravin <dubravin111@gmail.com>
2023-06-10 10:21:50 +0300

    add styles to table

--- components/screens/products/products-list/ProductList.tsx
+++ components/screens/products/products-list/ProductList.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useProducts } from './useProducts';
 import { IProduct } from '@/lib/types/product.types';
-
+import styles from './productList.module.css';
 const ProductList = () => {
   const { data, isLoading, isError } = useProducts();
 
@@ -12,22 +12,37 @@ const ProductList = () => {
   if (isError) {
     return <h1>Fail to load products...</h1>;
   }
-  console.log(data);
 
   return (
-    <div>
-      {Array.isArray(data) &&
-        data.map((product: IProduct) => {
-          return (
-            <div className="flex justify-between px-6">
-              <p>{product._id}</p>
-              <p>{product.title}</p>
-              <p>{product.description}</p>
-              <p>{product.price}</p>
-            </div>
-          );
-        })}
-    </div>
+    <table className={styles.table}>
+      <thead>
+        <tr>
+          <td>id</td>
+
+          <td>Name</td>
+
+          <td>Description</td>
+
+          <td>Price</td>
+        </tr>
+      </thead>
+      <tbody>
+        {data &&
+          data.map((product: IProduct) => {
+            return (
+              <>
+                <tr className={styles.table__row}>
+                  <td>{product._id}</td>
+                  <td>{product.title}</td>
+                  <td>{product.description}</td>
+                  <td>{product.price}</td>
+                </tr>
+                <tr className="h-3"></tr>
+              </>
+            );
+          })}
+      </tbody>
+    </table>
   );
 };
 
