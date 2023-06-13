cd16c872402590576e30a6bd147c92db89e4988a
Bohdan-Dubravin <dubravin111@gmail.com>
2023-06-13 17:53:29 +0300

    change upload layout

--- components/ui/input-fields/upload-input/UploadField.tsx
+++ components/ui/input-fields/upload-input/UploadField.tsx
@@ -29,40 +29,42 @@ const UploadField = ({
   const { uploadImage, isLoading } = useUpload(setImgList, setCurrentImages);
   return (
     <div className={cn(styles.field)} style={style}>
-      <label className={styles.label}>
-        {!isLoading && (
-          <HeroIcon className="text-gray-500 w-6" icon="ArrowUpTrayIcon" />
-        )}
-        <span>{isLoading ? <ClipLoader color="#6B7280" /> : placeholder}</span>
-        <input
-          className={styles.upload__input}
-          type="file"
-          onChange={uploadImage}
-        />
-
-        {error && <div>{error.message}</div>}
-      </label>
-
       <div className={styles.uploadImageContainer}>
         <h2 className={styles.title}>Drag and drop to change images order</h2>
+        <div className="flex gap-2 items-center flex-wrap">
+          <ReactSortable
+            className={styles.uploadImageList}
+            //@ts-ignore
+            list={imgList}
+            //@ts-ignore
+            setList={updateImgOrder}
+          >
+            {imgList && imgList[0] && Array.isArray(imgList) ? (
+              imgList.map((img) => (
+                <div key={img} className={styles.imgContainer}>
+                  <img src={img} alt="new img" className={styles.image} />
+                </div>
+              ))
+            ) : (
+              <h3>No image in this product add new one</h3>
+            )}
+          </ReactSortable>
+          <label className={styles.label}>
+            {!isLoading && (
+              <HeroIcon className="text-gray-500 w-6" icon="ArrowUpTrayIcon" />
+            )}
+            <span>
+              {isLoading ? <ClipLoader color="#6B7280" /> : placeholder}
+            </span>
+            <input
+              className={styles.upload__input}
+              type="file"
+              onChange={uploadImage}
+            />
 
-        <ReactSortable
-          className={styles.uploadImageList}
-          //@ts-ignore
-          list={imgList}
-          //@ts-ignore
-          setList={updateImgOrder}
-        >
-          {imgList && imgList[0] && Array.isArray(imgList) ? (
-            imgList.map((img) => (
-              <div key={img} className={styles.imgContainer}>
-                <img src={img} alt="new img" className={styles.image} />
-              </div>
-            ))
-          ) : (
-            <h3>No image in this product add new one</h3>
-          )}
-        </ReactSortable>
+            {error && <div>{error.message}</div>}
+          </label>
+        </div>
       </div>
     </div>
   );
